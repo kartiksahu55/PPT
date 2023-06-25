3a093a536c183e249015b82ced42fa55b7b80efb
Kartik Chandra Sahu <125174136+kartiksahu55@users.noreply.github.com>
2023-06-25 19:49:59 +0530

    Update Q1. Move Zeroes.js

--- PPT-Program/DSA - Mock Test 1/Q1. Move Zeroes.js
+++ PPT-Program/DSA - Mock Test 1/Q1. Move Zeroes.js
@@ -1,15 +1,14 @@
-function moveZeroes(nums) {
-    let right = 0;
-  
-    for (let left = 0; left < nums.length; left++) {
-      if (nums[left] !== 0) {
-        [nums[left], nums[right]] = [nums[right], nums[left]];
-        right++;
-      }
+function moveZero(nums) {
+  let right = 0;
+
+  for (let left = 0; left < nums.length; left++) {
+    if (nums[left] !== 0) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+      right++;
     }
   }
-  
-  const nums = [0, 1, 0, 3, 12];
-  moveZeroes(nums);
-  console.log(nums);
-  
+}
+
+const nums = [0, 1, 0, 3, 12];
+moveZero(nums);
+console.log(nums);  
